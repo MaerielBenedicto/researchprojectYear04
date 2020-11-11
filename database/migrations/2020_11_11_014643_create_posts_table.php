e1814ff13c158312e2dc7630afea795782318a4d
Maeriel Benedicto <N00150817@student.iadt.ie>
2020-11-11 01:52:55 +0000

    Forum and post migration

---
+++ database/migrations/2020_11_11_014643_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->string('title');
+          $table->text('body');
+          $table->date('date');
+          $table->bigInteger('user_id')->unsigned();
+          $table->bigInteger('forum_id')->unsigned();
+
+          $table->timestamps();
+
+          $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('restrict');
+          $table->foreign('forum_id')->references('id')->on('forums')->onUpdate('cascade')->onDelete('restrict');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
