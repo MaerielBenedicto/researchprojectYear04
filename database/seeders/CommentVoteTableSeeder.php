6508efd73f22061d2f420723eab6eeef9574481a
Maeriel Benedicto <N00150817@student.iadt.ie>
2021-01-13 04:21:49 +0000

    Created Vote seeders

---
+++ database/seeders/CommentVoteTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\CommentVote;
+use App\Models\User;
+use App\Models\Comment;
+
+
+class CommentVoteTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      $this->amntOfUsers = User::all()->count();
+      $this->amntOfComments = Comment::all()->count();
+
+      CommentVote::factory()
+              ->count(500)
+              ->create([
+                //assign random integer to user_id (amount of users)
+                'user_id' => function(){
+                  return mt_rand(1, $this->amntOfUsers);
+                },
+                'comment_id' => function(){
+                    return mt_rand(1, $this->amntOfComments);
+                  },
+              ]);
+
+    }
+}
---
+++ database/seeders/PostVoteSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\PostVote;
+use App\Models\User;
+use App\Models\Post;
+
+
+class PostVoteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      $this->amntOfUsers = User::all()->count();
+      $this->amntOfPosts = Post::all()->count();
+
+      PostVote::factory()
+              ->count(500)
+              ->create([
+                //assign random integer to user_id (amount of users)
+                'user_id' => function(){
+                  return mt_rand(1, $this->amntOfUsers);
+                },
+                'post_id' => function(){
+                    return mt_rand(1, $this->amntOfPosts);
+                  },
+              ]);
+
+    }
+}
