4672adeb5c94b59f66df5fae86751b2745caaabf
Maeriel Benedicto <N00150817@student.iadt.ie>
2021-03-24 23:32:12 +0000

    update database config

--- config/database.php
+++ config/database.php
@@ -40,7 +40,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '8889'),
-            'database' => 'cc_testing',
+            'database' => 'cc_db',
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
